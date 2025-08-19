f5ed7e60449782379eddf00f8e5deb869fbcdc30
jv222th <jv222th@student.lnu.se>
2025-08-19 19:21:28 +0200

    Update dbsettings and package.json

--- db/init/01_createdb.sql
+++ db/init/01_createdb.sql
@@ -1,26 +1,7 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema movie_db
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema movie_db
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `movie_db` DEFAULT CHARACTER SET utf8mb3 ;
-USE `movie_db` ;
-
 -- -----------------------------------------------------
 -- Table `movie_db`.`Actor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `movie_db`.`Actor` (
+CREATE TABLE IF NOT EXISTS `Actor` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `gender` INT NULL DEFAULT NULL,
@@ -28,25 +9,25 @@ CREATE TABLE IF NOT EXISTS `movie_db`.`Actor` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
 -- Table `movie_db`.`Genre`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `movie_db`.`Genre` (
+CREATE TABLE IF NOT EXISTS `Genre` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(500) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
 -- Table `movie_db`.`movie`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `movie_db`.`Movie` (
+CREATE TABLE IF NOT EXISTS `Movie` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(250) NOT NULL,
   `release_year` YEAR NULL DEFAULT NULL,
@@ -55,13 +36,13 @@ CREATE TABLE IF NOT EXISTS `movie_db`.`Movie` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
 -- Table `movie_db`.`movie_has_genre`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `movie_db`.`Movie_has_Genre` (
+CREATE TABLE IF NOT EXISTS `Movie_has_Genre` (
   `genre_id` INT NOT NULL,
   `movie_id` INT NOT NULL,
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -71,13 +52,13 @@ CREATE TABLE IF NOT EXISTS `movie_db`.`Movie_has_Genre` (
   INDEX `fk_Genre_has_Movie_Genre1_idx` (`genre_id` ASC) VISIBLE)
 ENGINE = InnoDB
 AUTO_INCREMENT = 455494
-DEFAULT CHARACTER SET = utf8mb3;
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
 -- Table `movie_db`.`rating`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `movie_db`.`Rating` (
+CREATE TABLE IF NOT EXISTS `Rating` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `rating` DECIMAL(2,1) NULL DEFAULT NULL,
@@ -87,17 +68,17 @@ CREATE TABLE IF NOT EXISTS `movie_db`.`Rating` (
   INDEX `fk_Rating_Movie1_idx` (`movie_id` ASC) VISIBLE,
   CONSTRAINT `fk_Rating_Movie1`
     FOREIGN KEY (`movie_id`)
-    REFERENCES `movie_db`.`Movie` (`id`)
+    REFERENCES `Movie` (`id`)
     ON DELETE CASCADE)
 ENGINE = InnoDB
 AUTO_INCREMENT = 145241
-DEFAULT CHARACTER SET = utf8mb3;
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
 -- Table `movie_db`.`role`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `movie_db`.`Role` (
+CREATE TABLE IF NOT EXISTS `Role` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `character_name` VARCHAR(500) NULL DEFAULT NULL,
   `actor_id` INT NOT NULL,
@@ -108,40 +89,40 @@ CREATE TABLE IF NOT EXISTS `movie_db`.`Role` (
   INDEX `fk_Role_Movie1_idx` (`movie_id` ASC) VISIBLE,
   CONSTRAINT `fk_Role_Actor`
     FOREIGN KEY (`actor_id`)
-    REFERENCES `movie_db`.`Actor` (`id`)
+    REFERENCES `Actor` (`id`)
     ON DELETE CASCADE,
   CONSTRAINT `fk_Role_Movie1`
     FOREIGN KEY (`movie_id`)
-    REFERENCES `movie_db`.`Movie` (`id`)
+    REFERENCES `Movie` (`id`)
     ON DELETE CASCADE)
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
 -- Table `movie_db`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `movie_db`.`User` (
+CREATE TABLE IF NOT EXISTS `User` (
   `id` CHAR(36) NOT NULL DEFAULT (UUID()),
   `username` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `unique_username` (`username` ASC) VISIBLE
 ) ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
 -- Fix the foreign keys.
 -- -----------------------------------------------------
-ALTER TABLE `movie_db`.`Movie_has_Genre`
+ALTER TABLE `Movie_has_Genre`
   ADD CONSTRAINT `fk_Genre_has_Movie_Genre1`
   FOREIGN KEY (`genre_id`)
-  REFERENCES `movie_db`.`Genre` (`id`)
+  REFERENCES `Genre` (`id`)
   ON DELETE CASCADE,
   ADD CONSTRAINT `fk_Genre_has_Movie_Movie1`
   FOREIGN KEY (`movie_id`)
-  REFERENCES `movie_db`.`Movie` (`id`)
+  REFERENCES `Movie` (`id`)
   ON DELETE CASCADE;
 
 
